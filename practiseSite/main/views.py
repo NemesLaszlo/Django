7418cd180974dddf8a78487c66bb43635250d4e4
NemesLaszlo <wow.laszlo@gmail.com>
2020-01-17 18:17:45 +0100

    Forms - CreateNewList

--- practiseSite/main/views.py
+++ practiseSite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 
 # Create your views here.
 
@@ -12,3 +13,18 @@ def index(response, id):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" % t.id)
+
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form": form})
